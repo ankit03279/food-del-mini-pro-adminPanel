ba95398849550979804c58acb6f507cef16a19ea
Ankit K Gautam <ankitanuj032@gmail.com>
2025-11-18 23:03:40 +0530

    Fix URL for the backend service

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,7 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'https://food-del-mini-projetc-back-1.onrender.com'
+export const url = 'https://food-del-mini-projetc-back.onrender.com'
 export const currency = '₹'
 
 export const assets ={
